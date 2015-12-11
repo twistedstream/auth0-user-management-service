0b4bbf182c05b94771132b01c26f74cf74022765
twistedstream <peter@twistedstream.com>
2015-12-10 22:13:10 -0600

    Webtask Express support with mock /users endpoint

--- webtask.js
+++ webtask.js
@@ -1,1 +1,13 @@
-//TODO: implement
+var jwt = require('express-jwt');
+var Express = require('express');
+var Webtask = require('webtask-tools');
+
+var app = Express();
+
+// endpoints
+
+app.get('/users', function (req, res) {
+  res.status(200).json({ foo: 'bar' });
+});
+
+module.exports = Webtask.fromExpress(app);
